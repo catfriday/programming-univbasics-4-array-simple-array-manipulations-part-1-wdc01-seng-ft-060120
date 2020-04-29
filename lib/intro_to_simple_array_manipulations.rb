91cb0fdad07149aaba33d894d0e012c3310d8577
Catrina Friday <cat.friday@gmail.com>
2020-04-29 03:03:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ end
 
 def shift_with_args(array)
   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @ice_cream_brands.shift(0)
+  @ice_cream_brands.shift(2)
 
 end 
 
