7dff515e969a93ea383f665db4fd3257e5a3e424
Catrina Friday <cat.friday@gmail.com>
2020-04-29 02:48:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,9 +18,11 @@ end
 def pop_with_args (array)
   @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
    @dog_breeds.pop(2)
-
 end 
 
-
+def using_unshift(array)
+  
+  
+end
 
 
